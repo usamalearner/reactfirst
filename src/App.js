9fc571c5f2c50ee061be153464517e48542fca48
usamalearner <syedusamalearner@gmail.com>
2020-06-11 01:43:21 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          by syed usama ali
         </p>
         <a
           className="App-link"
